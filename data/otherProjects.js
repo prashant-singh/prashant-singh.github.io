52b22e40133219ecc434a22c68aeb8890a95b3c6
Prashant Singh <prashant19194@gmail.com>
2022-11-01 22:00:13 +0530

    Revert "Revert "More projects added.""
    This reverts commit 26b01659b1be652211029079dc20f9e416520246.

---
+++ data/otherProjects.js
@@ -0,0 +1,199 @@
+const otherProjects = [
+	{
+		title: "MidNight Noise (Realtime Maps with GPS Tracking)",
+		description: `Realtime GPS tracking on Maps where you can connect with other players and attend live events on a pinned location.`,
+		techs: ["Unity3D", "C#", "OpenMapsSDK", "ios", "Android"],
+		links: [
+			{
+				platform: "Android",
+				link: "https://play.google.com/store/apps/details?id=com.midnightnoise"
+			},
+			{
+				platform: "IOS",
+				link: "https://apps.apple.com/ca/app/midnight-noise/id1490544831"
+			},
+			{
+				platform: "Website",
+				link: "https://midnightnoise.com/"
+			}
+		]
+	},
+	{
+		title: "Juice Team [Math Wizard] for Osmo",
+		description: `An addition, subtraction and multiplication adventure for ages 6-8! Add fruit pieces for the animals and play sports like Basketball, Soccer, Baseball and Relay Race.`,
+		techs: ["Unity3D", "C#", "iPad"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://www.playosmo.com/en/shopping/games/math-wizard-magical-workshop/"
+			}
+		]
+	},
+	{
+		title: "Lidl AquaMini",
+		description: `A fun way to teach children about the cleanliness of our Oceans so the Fishes can live freely.`,
+		techs: ["Unity3D", "C#", "IOS"],
+		links: [
+			{
+				platform: "Android",
+				link: "https://play.google.com/store/apps/details?id=com.lidl.aquamini&hl=en_IN/"
+			}
+		]
+	},
+	{
+		title: "BBBlackjack",
+		description: `Blackjack game where you can create clubs with your friends and create tables with your chosen game settings.`,
+		techs: ["Unity3D", "C#", "IOS"],
+		links: [
+		]
+	},
+	{
+		title: "UDLE for Zydus Cadila",
+		description: `Learning based Board game for Zydus Cadila internal employees to learn about new products more fun way.`,
+		techs: ["Unity3D", "C#", "Android"],
+		links: [
+			{
+				platform: "Android",
+				link: "https://play.google.com/store/apps/details?id=com.zyduscadila.udle"
+			}
+		]
+	},
+	{
+		title: "T.H.I.S.",
+		description: `T.H.I.S is a little minimalist game where your goal is to, well, help the ball reach the goal.`,
+		techs: ["Unity3D", "C#", "Android", "WindowsPhone"],
+		links: [
+			{
+				platform: "Android",
+				link: "https://play.google.com/store/apps/details?id=com.madperception.thisgame"
+			},
+			{
+				platform: "Windows Phone",
+				link: "https://www.microsoft.com/en-in/p/this/9nblggh5kj2x"
+			},
+			{
+				platform: "Website",
+				link: "https://madperception.github.io/"
+			}
+		]
+	},
+	{
+		title: "Speed Match for Lumosity",
+		description: `A fast-paced exercise, Speed Match exercises your brain’s ability to process information and thus improves a wide variety of cognitive skills.`,
+		techs: ["Unity3D", "C#"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://www.lumosity.com/en/brain-games/speed-match/"
+			}
+		]
+	},
+	{
+		title: "Lost In Migration for Lumosity",
+		description: `Lost in Migration exercises your attention abilities. Games like Lost in Migration can boost your focus, helping you avoid distraction and improve productivity at work.`,
+		techs: ["Unity3D", "C#"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://www.lumosity.com/en/brain-games/lost-in-migration/"
+			}
+		]
+	},
+	{
+		title: "Memory Match for Lumosity",
+		description: `In Memory Match, you must quickly determine whether a flashcard symbol matches the one presented 2 times previously.`,
+		techs: ["Unity3D", "C#"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://www.lumosity.com/en/brain-games/memory-match/"
+			}
+		]
+	},
+	{
+		title: "Enduvo VR Systems",
+		description: `Enduvo is the immersive content authoring and delivery platform that removes the complexity, inflexibility, and high cost associated with creating virtual and augmented reality content.`,
+		techs: ["Unity3D", "C#","HTC Vive"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://enduvo.com/"
+			}
+		]
+	},
+	{
+		title: "Liquor Store",
+		description: `It's a virtual store where you can walk around in the liquor store and grab liquor bottles from shelf and put in your always following you around cart and at the checkout counter you can place the order. Also it has analytics system that generates a full report of behaviour.`,
+		techs: ["Unity3D", "C#","HTC Vive"],
+		links: [
+			{
+				platform: "Video",
+				link: "https://www.youtube.com/watch?v=RnKUMQ5DjhY"
+			}
+		]
+	},
+	{
+		title: "VR Mall",
+		description: `A virtual mall where you can roam around and pick stuff with your hands and put in the cart and checkout the order yourself.`,
+		techs: ["Unity3D", "C#","HTC Vive"],
+		links: [
+			{
+				platform: "Video",
+				link: "https://www.youtube.com/watch?v=VE0sh2bXK6Y"
+			}
+		]
+	},
+	{
+		title: "Skill Games [Touchscreen Table]",
+		description: `A 55" touch screen table in which 4 players can sit around it and insert the dollar bills in the bill accepter attached to the table and play the skill games against the store.`,
+		techs: ["Unity3D", "C#"],
+		links: [
+		]
+	},
+	{
+		title: "Localization Plugin for Unity",
+		description: `An open source plugin that will help you localize your project with different languages that is easily manageable and also works with runtime language change.`,
+		techs: ["Unity3D", "C#","Plugin"],
+		links: [
+			{
+				platform: "Github",
+				link: "https://github.com/prashant-singh/localization"
+			}
+		]
+	},
+	{
+		title: "ExpertJet",
+		description: `Flight management app where you can see your booked private flyexpertjet.com itineraries and legs.`,
+		techs: ["Unity3D", "C#","IOS"],
+		links: [
+			{
+				platform: "Website",
+				link: "https://www.flyexpertjet.com/mobile-app/"
+			}
+		]
+	},
+	{
+		title: "Scripts Manager",
+		description: `Gives the list of scripts in the current scene with gameobject reference..`,
+		techs: ["Unity3D", "C#","EditorScript"],
+		links: [
+			{
+				platform: "Github",
+				link: "https://github.com/prashant-singh/scripts-manager-unity3d"
+			}
+		]
+	},
+	{
+		title: "Camera Bound 2D",
+		description: `A camera bounds visualizer editor tool.`,
+		techs: ["Unity3D", "C#","EditorScript"],
+		links: [
+			{
+				platform: "Github",
+				link: "https://github.com/prashant-singh/camera-bounds-2D"
+			}
+		]
+	}
+];
+
+export default otherProjects;
---
+++ pages/resume.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="px-4  mx-auto sm:px-6 xl:max-w-6xl xl:px-0">
+		<main class="relative mb-auto">
+			<h2 class="my-4 ml-2 text-3xl text-gray-700 font-bold capitalize dark:text-blue-200">
+				All Projects ({{otherProjects.length}})
+			</h2>
+			<hr class="my-8 h-px bg-gray-800 border-0 dark:bg-gray-700">
+
+			<div class="container mx-auto py-4">
+				<div class="-m-4">
+					<resume-card v-for="(item,index) in otherProjects" :key="item.title" :title="item.title"
+						:description="item.description" :links="item.links" :techs="item.techs" :index="index">
+					</resume-card>
+					
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+  
+<script>
+import otherProjects from "../data/otherProjects";
+import siteMetaInfo from "../data/sitemetainfo";
+export default {
+	data() {
+		return {
+			otherProjects: otherProjects,
+			siteMetaInfo: siteMetaInfo,
+		};
+	},
+	head: {
+		title: [siteMetaInfo.author] + " | All Projects",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{
+				hid: "description",
+				name: "description",
+				content: siteMetaInfo.description,
+			},
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
+};
+</script>
