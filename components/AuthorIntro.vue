e3b758de1e3b491252bc9259da3ee6c72411fa3f
Prashant Singh <prashant19194@gmail.com>
2023-05-13 14:55:49 +0530

    Update AuthorIntro.vue

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="mb-6">
     <h2 class="mt-6 text-2xl text-gray-700 font-bold capitalize dark:text-blue-200">
-      👋 Hey , Good Day !
+      👋 Hi!
     </h2>
     <p class="mt-8 dark:bg-gray-800 dark:text-blue-200 text-justify">
-      I am a video game developer with 7+ years of programming experience in Unity3D Engine.
-Have worked on mobile games as well as Windows Standalone and VR Devices like HTC Vive and Oculus GO.
-Created some awesome editor scripts to make so many tasks easy for developers, you can checkout my github repos.
+    Experienced Unity3D video game developer with a focus on mobile, Windows Standalone, and VR platforms. Skilled in creating engaging games and developing editor scripts for streamlined development. Passionate about pushing the boundaries of the gaming industry.
     </p>
   </div>
 </template>
