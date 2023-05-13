e8d01929a604d2104ef8c83c9fc00fe1c02df7f4
prashant-singh <prashant-singh@users.noreply.github.com>
2023-05-13 09:42:17 +0000

    deploy: 314b691ad18d3106305702cc5151db51b1138368

---
+++ _nuxt/static/1683970935/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683970935",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}},_img:{"/_ipx/_/profile.png":"\u002F_nuxt\u002Fimage\u002F0c98a6.png"}}}(null,"\u002F"));
